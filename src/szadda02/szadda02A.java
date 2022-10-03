e2ae0d1c712ca7c0a6fca2ff46924bd8e6662c06
sweetyzadda <vsksowmyazadda@gmail.com>
2022-10-03 19:31:16 +0530

    committing love

--- src/szadda02/szadda02A.java
+++ src/szadda02/szadda02A.java
@@ -5,6 +5,8 @@ public class szadda02A {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("hello 02nd commit");
+        String love = "nanamma";
+        System.out.println("hello 3rd commit" + love);
     }
 
 }
