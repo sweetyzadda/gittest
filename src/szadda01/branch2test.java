6a6c7c852c87dd283979561e3b6e577260333ed0
sweetyzadda <vsksowmyazadda@gmail.com>
2022-10-04 12:10:10 +0530

    pushing branch2 changes

---
+++ src/szadda01/branch2test.java
@@ -0,0 +1,12 @@
+package szadda01;
+
+public class branch2test {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("new file");
+        String a = "potato";
+        System.out.println("new file" + " " + a);
+    }
+
+}
